dd9dbbfe5729d940478a617e648452d4c9138bee
R. S. Coldwater <lucas15azevedo@gmail.com>
2023-05-13 19:08:03 -0300

    Criação git

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const oper = document.querySelector("#taOperacao");
+    oper.addEventListener("input", function apenasPermitidos(event){
+        console.log(event.keyCode);
+        
+        // if()
+    })
