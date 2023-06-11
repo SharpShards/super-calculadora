9e13297d1cdf09999bba0a2dac7e1082d8d18c36
sharp shards <lucas15azevedo@gmail.com>
2023-06-11 15:21:30 -0300

    Ajeitei restrições de combinações de caracteres e adicionei pontos para separar o número em classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,63 @@
+let termo = "";
+
+function atualizarDisplay(carac){
+    display = oper.value;
+
+    // Apagar tudo
+    if(carac == "c"){
+        oper.value = "";
+        termo = "";
+
+        return;
+    }
+
+    // Limite de caracteres
+    if(termo.length == 19 || // Número maior que 15 dígitos + 4 pontos
+    display == 200){ // Caractere total no display
+        return;
+    } 
+
+    // Verificar se é um 0 + Número
+    if(carac > 0 && // Próximo caractere for um número
+    termo[0] == 0 // Caractere antreior for 0
+    ){
+        console.log(oper.value[display.lastIndexOf(0)]);
+
+        oper.value = display.replace(display[display.lastIndexOf(0)], "");
+        termo = oper.value;
+    }
+
+    // Atualizar
+    if(carac != "c"){
+        oper.value += carac;
+    }
+
+    // Formatar
+    if(display.length == 2 ||
+    display.length == 6 ||
+    display.length == 10 ||
+    display.length == 14){    
+        oper.value += ".";        
+        termo += "."
+    }
+    
+    // Identificar termos
+    if(carac == "+" ||
+    carac == "-" ||
+    carac == "/" ||
+    carac == "x" ||
+    carac == "c"){
+        termo = "";
+    }else{
+        termo += carac;
+    }
+}
+
 const oper  = document.querySelector("#taOperacao");
 
 const c = document.querySelector("main #sctBasica #sctButtons #dvBasico button:nth-child(4)");
     c.addEventListener("click", function apagarCarac(event){
-        oper.value = "";
+        atualizarDisplay("c");
     })
 
 const plus = document.querySelector("main #sctBasica #sctButtons #dvBasico button:nth-child(20)");
@@ -10,13 +65,13 @@ const plus = document.querySelector("main #sctBasica #sctButtons #dvBasico butto
         last = oper.value[oper.value.length - 1];
 
         if(last == "+" ||
-            last == "-" ||
-            last == "x" ||
-            last == "/"){ 
+        last == "-" ||
+        last == "x" ||
+        last == "/" ||
+        oper.value.length == 0){     
             event.preventDefault();
-            console.log('Lucas');
         }else{
-            oper.value += "+";
+            atualizarDisplay("+");
         }        
     })
 
@@ -25,12 +80,13 @@ const minus = document.querySelector("main #sctBasica #sctButtons #dvBasico butt
         last = oper.value[oper.value.length - 1];
 
         if(last == "-" ||
-            last == "+" ||
-            last == "x" ||
-            last == "/"){
+        last == "+" ||
+        last == "x" ||
+        last == "/" ||
+        oper.value.length == 0){
             event.preventDefault();
-        }else{
-            oper.value += "-";
+        }else{            
+            atualizarDisplay("-");
         }        
     })
 
@@ -39,12 +95,13 @@ const mult = document.querySelector("main #sctBasica #sctButtons #dvBasico butto
         last = oper.value[oper.value.length - 1];
 
         if(last == "x" ||
-            last == "+" ||
-            last == "-" ||
-            last == "/"){
+        last == "+" ||
+        last == "-" ||
+        last == "/" ||
+        oper.value.length == 0){
             event.preventDefault();
-        }else{
-            oper.value += "x";
+        }else{                    
+            atualizarDisplay("x");
         }        
     })
 
@@ -53,12 +110,13 @@ const div = document.querySelector("main #sctBasica #sctButtons #dvBasico button
         last = oper.value[oper.value.length - 1];
 
         if(last == "/" ||
-            last == "+" ||
-            last == "-" ||
-            last == "x"){
+        last == "+" ||
+        last == "-" ||
+        last == "x" ||
+        oper.value.length == 0){
             event.preventDefault();
-        }else{
-            oper.value += "/";
+        }else{            
+            atualizarDisplay("/");
         }        
     })
 
@@ -67,12 +125,40 @@ const zero = document.querySelector("main #sctBasica #sctButtons #dvBasico butto
         last = oper.value[oper.value.length - 2] + "" + "" + oper.value[oper.value.length - 1];
 
         if((oper.value.length == 1 && last == "undefined0") || // Só há um caractere e é 0
-            last == "+0" ||
-            last == "-0" ||
-            last == "x0" ||
-            last == "/0"){
+        last == "+0" ||
+        last == "-0" ||
+        last == "x0" ||
+        last == "/0"){
             event.preventDefault();
-        }else{
-            oper.value += "0";
-        }        
+        }else{            
+            atualizarDisplay("0");
+        }     
     })
+
+const um = document.querySelector("#btnUm");
+    um.addEventListener("click", function adicionarZero(event){
+        atualizarDisplay("1");
+    })
+
+const vir = document.querySelector("main #sctBasica #sctButtons #dvBasico button:nth-child(18)");
+    vir.addEventListener("click", function adicionarVirgula(event){
+        last = oper.value[oper.value.length - 1];        
+
+        if(last == "," ||
+        last == "-" ||
+        last == "x" ||
+        last == "/" ||
+        last == "+" ||
+        termo.indexOf(',') > 0){ // Um mesmo número não pode ter mais de uma vírgula.
+            event.preventDefault();
+        }else if(oper.value.length == 0){
+            oper.value = "0,";
+        }else{            
+            atualizarDisplay(",")
+        }
+    })
+
+// Se colocar zero antes de um número, o número substitui o zero
+// tirar ponto antes de sinal de operação
+// pontos estão dando problemas
+// 
